1acc45424e17bf0d0e2a4839ecff168ddf3d2b52
eloifabius <fabius1995>
2020-03-28 06:15:38 +0100

    creation des test unitaire

---
+++ src/test/java/fr/uvsq/pglp/CompositeTest.java
@@ -0,0 +1,15 @@
+package fr.uvsq.pglp;
+
+import org.junit.Test;
+
+public class CompositeTest {
+    @Test
+    <CompositePersonn>
+    void test(){
+        Personnels person1 = new Personnels.Builder().build();
+        CompositePersonn group;
+
+
+    }
+
+}
